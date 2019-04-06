a5a051a74852551b3167c543cd5e975ee75108e8
Oliver Falk <oliver@linux-kernel.at>
2019-04-06 17:46:23 +0200

    Adapt settings for new Django/new Python (module)

--- pfadi/settings.py
+++ pfadi/settings.py
@@ -39,20 +39,19 @@ INSTALLED_APPS = [
   'django.contrib.sessions',
   'django.contrib.messages',
   'django.contrib.staticfiles',
-  'social.apps.django_app.default',
+  'social_django',
   'django_extensions',
   'django_gravatar',
   'bootstrapform',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -70,8 +69,8 @@ TEMPLATES = [
               'django.template.context_processors.request',
               'django.contrib.auth.context_processors.auth',
               'django.contrib.messages.context_processors.messages',
-              'social.apps.django_app.context_processors.backends',
-              'social.apps.django_app.context_processors.login_redirect'
+              'social_django.context_processors.backends',
+              'social_django.context_processors.login_redirect'
             ],
         },
     },
@@ -137,7 +136,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = None
+TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
@@ -158,9 +157,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, ".", "static")
 STATIC_URL = '/static/'
 
 AUTHENTICATION_BACKENDS = (
-  'social.backends.facebook.FacebookOAuth2',
-  'social.backends.twitter.TwitterOAuth',
-  'social.backends.google.GoogleOAuth2',
+  'social_core.backends.facebook.FacebookOAuth2',
+  'social_core.backends.google.GoogleOAuth2',
   'django.contrib.auth.backends.ModelBackend',
 )
 SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
@@ -169,19 +167,21 @@ SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
   'fields': 'id,name,email',
 }
-SOCIAL_AUTH_TWITTER_SCOPE = ['email', ]
 
 SOCIAL_AUTH_PIPELINE = (
-  'social.pipeline.social_auth.social_details',
-  'social.pipeline.social_auth.social_uid',
-  'social.pipeline.social_auth.auth_allowed',
-  'social.pipeline.social_auth.social_user',
-  'social.pipeline.user.get_username',
-  'social.pipeline.social_auth.associate_by_email',
+  'social_core.pipeline.social_auth.social_details',
+  'social_core.pipeline.social_auth.social_uid',
+  'social_core.pipeline.social_auth.auth_allowed',
+  'social_core.pipeline.social_auth.social_user',
+  'social_core.pipeline.user.get_username',
+  'social_core.pipeline.social_auth.associate_by_email',
   # 'users.pipeline.require_email',
-  'social.pipeline.mail.mail_validation',
-  'social.pipeline.user.create_user',
-  'social.pipeline.social_auth.associate_user',
-  'social.pipeline.social_auth.load_extra_data',
-  'social.pipeline.user.user_details'
+  'social_core.pipeline.mail.mail_validation',
+  'social_core.pipeline.user.create_user',
+  'social_core.pipeline.social_auth.associate_user',
+  'social_core.pipeline.social_auth.load_extra_data',
+  'social_core.pipeline.user.user_details'
 )
+
+GRAVATAR_URL = 'https://libravatar.org/'
+GRAVATAR_SECURE_URL = 'https://libravatar.org/'
